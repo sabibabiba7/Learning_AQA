436cd8a9e2aa5bf5c9e861bbb87d70063cb784fb
astonuser <sabibabiba7@gmail.com>
2024-04-22 08:48:19 +0300

    First commit

---
+++ MODULE_1/src/main/java/com/FOR_AQA_1/FirstApp.java
@@ -0,0 +1,7 @@
+package com.FOR_AQA_1;
+
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ MODULE_1/src/main/java/school/lesson1/TryAgain.java
@@ -0,0 +1,9 @@
+package school.lesson1;
+
+public class TryAgain {
+    public static void main(String[] args) {
+        System.out.println("banana");
+        System.out.println("orange");
+        System.out.println("apple");
+    }
+}
