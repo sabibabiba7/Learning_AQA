62259246dcc45b2ef5c647624209bd73281948a7
astonuser <sabibabiba7@gmail.com>
2024-05-05 19:16:08 +0300

    lesson3

---
+++ MODULE_1/src/main/java/lesson3/Employees.java
@@ -0,0 +1,29 @@
+package lesson3;
+
+public class Employees {
+    private static String Surname;
+        private String Name;
+        private String FatherName;
+        private String position;
+        private String email;
+        private String phone;
+        private String salary;
+        private int age;
+public int getAge() {
+    return age;
+}
+public Employees(String Surname, String Name, String FatherName, String position, String email, String phone, String salary, String Age)
+        {
+            this.Surname = Surname;
+            this.Name = Name;
+            this.FatherName = FatherName;
+            this.position = position;
+            this.email = email;
+            this.phone = phone;
+            this.salary = salary;
+            this.age = age;
+        }
+public void GetInfo () {
+    System.out.println("Surname"+Surname+ "Name"+ Name+"FatherName"+FatherName+"Position"+position+"Email"+email+"Phone"+phone+"Salary"+salary+"Age"+age);
+    }
+}
