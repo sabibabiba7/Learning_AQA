d1c04c118820c09a294306851062df168b589caf
sabibabiba7 <167543235+sabibabiba7@users.noreply.github.com>
2024-06-10 11:40:52 +0500

    Update Cat.java

--- MODULE_1/src/main/java/Animal/Cat.java
+++ MODULE_1/src/main/java/Animal/Cat.java
@@ -1,15 +1,25 @@
-package Animal;
-public class Cat {
-    public class Cat extends Animal {
-        private static int count;
-        public Cat(String type, String name, int flying, int swims, int goes, int run, int crawling) {
-            this.type = type;
-            this.name = name;
-            this.swims = swims;
-            this.run = run;
-            count++;
-        public static int getCount() {
-            return count;
+package animals;
+
+public class Cat extends Animal {
+    private static final int MAX_DIST = 200;
+    private static int counter = 0;
+    @Override
+    public void run(int distance) {
+        if (distance > MAX_DIST) {
+            System.out.println("Кот: '" + getName() + "' столько не пробежит");
+        } else if (distance < 0) {
+            System.out.println("Расстояние не может быть меньше нуля");
+        } else if (distance == 0) {
+            System.out.println("Кот: '" + getName() + "' не сдвинулся с места");
+        } else {
+            System.out.println("Кот: '" + getName() + "' пробежал: " + distance);
         }
     }
-}
+    @Override
+    public void swim(int distance) {
+        System.out.println("В этой программе коты не плавают");
+    }
+
+    public static int catCounter() {
+        return counter;
+    }
