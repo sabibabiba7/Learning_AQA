31a36577da1aef8646ed41f02258ce768e955d54
sabibabiba7 <167543235+sabibabiba7@users.noreply.github.com>
2024-06-10 11:37:11 +0500

    Update Animal.java

--- MODULE_1/src/main/java/Animal/Animal.java
+++ MODULE_1/src/main/java/Animal/Animal.java
@@ -1,49 +1,23 @@
-package Animal;
+package animals;
 
 public class Animal {
-        protected String type;
-        protected String name;
-        protected int flying;
-        protected int swims;
-        protected int goes;
-        protected int run;
-        protected int crawling;
-        private static int count;
+    private String name;
+    private static int counter = 0;
 
-        public Animal() {
-            count++;
-        }
-        public static int getCount() {
-            return count;
-        }
-        @Override
-        protected Object clone() throws CloneNotSupportedException {
-            count++;
-            return super.clone();
-        }
-
-        @Override
-        public String toString() {
-            return "Вид/порода: '" + type + '\'' +
-                    ", Кличка: '" + name + "\' \nТТХ:" +
-                    ", Может проплыть " + swims + "м. " +
-                    ", Может пробежать " + run + "м. " +
-        }
-
-        protected void run(int distance) {
-            if (run >= distance) {
-                System.out.println(this.type + " по кличке " + this.name + "  - Пробежал " + distance + "м.");
-            } else {
-                System.out.println(this.type + " по кличке " + this.name + " ---- Не пробежал ---- ");
-            }
-        }
-
-        protected void swim(int distance) {
-            if (swims >= distance) {
-                System.out.println(this.type + " по кличке " + this.name + "   - Проплыл " + distance + "м.");
-            } else {
-                System.out.println(this.type + " по кличке " + this.name + " ---- Не умеет плавать ---- ");
-            }
-        }
+    public Animal(String name) {
+        this.name = name;
+        counter++;
+    }
+    public String getName() {
+        return name;
+    }
+    public void run(int distance) {
+        System.out.println("Животное бегает");
+    }
+    public void swim(int distance) {
+        System.out.println("Животное плавает");
+    }
+    public static int getCounter() {
+        return counter;
     }
 }
